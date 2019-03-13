737d2a873f54754e8f10c0c7062bf33a81e86c7c
jingyi1997 <jingyi@mail.nankai.edu.cn>
2019-03-13 11:18:28 +0800

    add_test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,19 @@
 export PYTHONPATH=$ROOT:$PYTHONPATH
 now=$(date +"%Y%m%d_%H%M%S")
-start_epoch=18
+start_epoch=16
 step=1
 end_epoch=22
+model_folder="experiment/softmax/checkpoints/"
 while test $start_epoch -le $end_epoch; do
   model_name="epoch_${start_epoch}_ckpt.pth.tar"
   echo "testing: "$model_name
-  GLOG_vmodule=MemcachedClient=-1 OMPI_MCA_btl_smcuda_use_cuda_ipc=0 OMPI_MCA_mpi_warn_on_fork=0 \
+  if [ -f ${model_folder}${model_name} ]; then 
+    GLOG_vmodule=MemcachedClient=-1 OMPI_MCA_btl_smcuda_use_cuda_ipc=0 OMPI_MCA_mpi_warn_on_fork=0 \
     srun --mpi=pmi2 --job-name $1 --partition=HA_vechicle -n1 --gres=gpu:8 --ntasks-per-node=8 \
         python -u eval_lfw.py --loss_type a-softmax --ckpt experiment/a-softmax --lfw /mnt/lustre/xujingyi/sphereface/preprocess/result/lfw-112X96 --model=$start_epoch
-  ((start_epoch+=step))
+    ((start_epoch+=step))
+  else
+    echo "sleep"
+    sleep 30m
+  fi
 done
